7c0f9fd22a1533457f4df6aab6dcffe8c9c429dd
Suyash Wagh <suyashcr07@gmail.com>
2022-02-12 17:05:09 +0530

    view change

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Imports
 const express = require('express');
+const expressSession = require('express-session');
 const expressLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
 const path = require('path');
 
 // Port
@@ -8,15 +10,32 @@ const port = 8000;
 
 const app = express();
 
-// Body parser
-app.use(express.urlencoded({ extended: false }));
-
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+
+// Express-session
+app.use(expressSession({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// Flash for notifications
+app.use(flash());
+
+app.use(function (req, res, next) {
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+});
 
 // Routes
 app.use('/', require('./routes'));
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const db = require('../config/mongoose');
+const flash = require('connect-flash');
+const User = require('../models/User');
 
 router.get('/login', (req, res) => {
 	return res.render('login');
@@ -6,6 +9,24 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
 	return res.render('register');
+});
+
+router.post('/login', (req, res) => {
+	console.log(req.body);
+	const { email, password } = req.body;
+
+	return res.redirect('back');
+});
+
+router.post('/register', (req, res) => {
+	console.log(req.body);
+
+	const { firstName, lastName, email, password, confirmPassword } = req.body;
+
+	if (password !== confirmPassword) {
+		return res.redirect('back');
+	}
+
 });
 
 module.exports = router;
