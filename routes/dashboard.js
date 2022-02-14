e9023e0cd16f8799179ef4e6a75336039f81602a
Suyash Wagh <suyashcr07@gmail.com>
2022-02-14 13:03:23 +0530

    Add favicon, fix dynamic date issue, add navbar, add logout functionality

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -9,28 +9,37 @@ router.get('/:id', async (req, res) => {
 		let user = await User.findById(id);
 		let habits = await Habit.find({user: id});
 
-		// for (let i = 0; i < habits.length; i++) {
+		for (let i = 0; i < habits.length; i++) {
 
-		// 	if (habits[i].dates[6].Date.toDateString('en-IN') !== new Date().toDateString('en-In')) {
-		// 		// number of days since habit was created
-		// 		let numberOfDays = Math.round((new Date() - habits[i].createdAt) / (1000*60*60*24));
+			if (habits[i].dates[6].Date.toDateString() !== new Date().toDateString()) {
+				// number of days since habit was created
+				let numberOfDays = Math.round((new Date() - habits[i].createdAt) / (1000*60*60*24));
 
-		// 		if (numberOfDays > 0) {
-		// 			habits[i].dates.splice(numberOfDays);
+				if (numberOfDays > 0) {
+					habits[i].dates = habits[i].dates.splice(numberOfDays);
 
-		// 			for (let j = numberOfDays - 1; j >= 0; j--) {
-		// 				let newDate = new Date(new Date().setDate(new Date().getDate() - j));
-		// 				habits[i].dates.push({Date: newDate, Status: 'None'});
-		// 			}
+					for (let j = numberOfDays - 1; j >= 0; j--) {
+						let newDate = new Date(new Date().setDate(new Date().getDate() - j));
+						habits[i].dates.push({Date: newDate, Status: 'None'});
+					}
+					await habits[i].save();
+				}
+			}
+		}
 
-		// 			await habits[i].save();
-		// 		}
-		// 	}
-		// }
+		// console.log('before splice: ', habits[0].dates);
+		// habits[0].dates = habits[0].dates.splice(2);
+		// console.log('after splice: ', habits[0].dates);
+		// habits[0].dates.push({Date: new Date(), Status: 'None'});
+		// console.log('after push: ', habits[0].dates);
+		// habits[0].save();
+		// console.log('after db save: ', habits[0].dates);
+
 
 		return res.render('dashboard', {
+			userName: user.firstName,
 			userId: id,
-			view: user.view,
+			view: 'weekly',
 			habits,
 		});
 	} catch (error) {
